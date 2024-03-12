45c95212aaea0e68c5bb239b1c9e510a35cf7fa7
Charles Feval <fevalc>
2024-03-12 14:06:33 -0700

    Initial Commit

---
+++ ScrumSucks/ScrumSucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumSucksApp.swift
+//  ScrumSucks
+//
+//  Created by Charles Feval on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct ScrumSucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
